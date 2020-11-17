bb11bac185c7502a6aec9750ed02c57872c72cdb
reinepoire <56972040+reinepoire@users.noreply.github.com>
2020-11-17 09:17:30 +0100

    ALTERNER MODES

---
+++ js/main.js
@@ -0,0 +1,25 @@
+//  DARK/LIGHT MODE
+function changeCss() {
+    var clicked = document
+    .getElementById('pagestyle')
+    .classList[0] === 'dark';
+  
+    var element = document.getElementById('pagestyle');
+    var  icone = document.body;
+  
+    if (clicked) {
+     element.href = 'css/light.css';
+     element.classList.remove('dark')
+     element.classList.add('light');
+     icone.classList.remove('dark')
+     icone.classList.add('light');
+     
+    } else {
+     element.href = 'css/dark.css';
+     element.classList.remove('light')
+     element.classList.add('dark');
+     icone.classList.remove('light')
+     icone.classList.add('dark');
+    }
+  }
+//  DARK/LIGHT MODE
